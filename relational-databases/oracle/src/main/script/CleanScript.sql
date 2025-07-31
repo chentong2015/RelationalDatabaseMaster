070b8d6ed1cf0ed1eda1f08b304c1013879fb218
chent5 <chen2015tong@gmail.com>
2025-07-31 16:28:59 +0200

    refresh database monitoring

--- relational-databases/oracle/src/main/script/CleanScript.sql
+++ relational-databases/oracle/src/main/script/CleanScript.sql
@@ -1,14 +1,11 @@
-TRUNCATE TABLE -- 只是删除表中所的数据，并不会删除表本省
-DROP TABLE     -- 删除表以及表中的所有数据
-
--- TODO. 先选择具有特定名称的表，然后执行删除
+-- TODO. 先选择具有特定名称的表，然后执行所有Drop命令
 select * from all_tables where table_name like 'TAB%'
 
 select 'DROP TABLE "' || TABLE_NAME || '" CASCADE CONSTRAINTS PURGE;'
 from all_tables
 where TABLE_NAME like 'TAB%';
 
--- TODO. 遍历删除Oracle中所有用户创建的数据表
+-- TODO. 遍历删除Oracle中所有用户创建的表
 BEGIN
     FOR c IN (SELECT table_name FROM user_tables) LOOP
         EXECUTE IMMEDIATE ('DROP TABLE "' || c.table_name || '" CASCADE CONSTRAINTS');
