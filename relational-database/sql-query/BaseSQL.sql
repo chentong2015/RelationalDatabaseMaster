859825ed6f9a31e9b3a58a46ddde6733535048e7
chent5 <chen2015tong@gmail.com>
2025-10-10 15:22:23 +0200

    refresh base sql

--- relational-database/sql-query/BaseSQL.sql
+++ relational-database/sql-query/BaseSQL.sql
@@ -1,9 +1,8 @@
 TRUNCATE TABLE -- 只是删除表中所的数据，并不会删除表本省
 DROP TABLE     -- 删除表以及表中的所有数据
 
--- TODO. Select 1 用于判断存在性, 不关系具体数据
--- 返回查询常数1,	测试存在性/性能优化/简单测试
--- 查询的语义更加明确, 性能可能会更快
+-- TODO. Select 1 用于判断存在性, 兼容各种数据库
+-- 返回查询常数1, 简单测试存在性, 语义更加明确, 性能可能会更快
 SELECT 1 FROM users WHERE email = 'test@example.com' LIMIT 1;
 
 -- UNION联合查询结果时必须保证字段一致
