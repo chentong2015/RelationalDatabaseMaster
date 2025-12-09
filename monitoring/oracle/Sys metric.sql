decd6d1c00eef9add59bcb696858d6d75dd32732
chent5 <chen2015tong@gmail.com>
2025-12-09 15:47:23 +0100

    refresh database monitoring

--- monitoring/oracle/Sys metric.sql
+++ monitoring/oracle/Sys metric.sql
@@ -5,3 +5,7 @@ SELECT metric_name, value
 FROM v$sysmetric
 WHERE metric_name = 'User Transaction Per Sec'
 AND rownum = 1;
+
+-- 查看Table(Insert, Update, Delete)更新次数
+-- 分析事务提交次数(累计值)和活跃度
+SELECT * FROM user_tab_modifications;
