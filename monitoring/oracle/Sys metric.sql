97285474757c809cbc06f209cb3b161dc017990d
chent5 <chen2015tong@gmail.com>
2025-12-30 17:15:38 +0100

    update sql optimization

--- monitoring/oracle/Sys metric.sql
+++ monitoring/oracle/Sys metric.sql
@@ -1,4 +1,4 @@
--- TODO: Oracle性能视图V$SYSMETRIC
+-- TODO: V$SYSMETRIC性能视图
 -- 实时TPS视图: 每分钟或15秒滑动窗口内的平均TPS
 -- 测试Batch批量导入: 每秒平均执行的事务提交
 SELECT metric_name, value
--- relational-database/db_queries/BaseSQL.sql
+++ relational-database/db_queries/BaseSQL.sql
@@ -1,7 +1,7 @@
 TRUNCATE TABLE -- 只是删除表中数据: 注意可能存在外键导致无法删除
 DROP TABLE     -- 删除表以及表中数据
 
--- TODO. Select 1 用于判断存在性, 兼容各种数据库
+-- Select 1 用于判断存在性, 兼容各种数据库
 -- 返回查询常数1, 简单测试存在性, 语义更加明确, 性能可能更快
 SELECT 1 FROM users WHERE email = 'test@example.com' LIMIT 1;
 
@@ -10,6 +10,7 @@ select id, name from tableA where id < 10
 UNION
 select id from tableA where id > 100;
 
--- TODO. 查询或统计离散数据
 select DISTINCT id from tableA where status = 'updated';
+
+-- 可以直接统计查询的离散字段
 select count(DISTINCT id) from tableA where status = 'updated';
