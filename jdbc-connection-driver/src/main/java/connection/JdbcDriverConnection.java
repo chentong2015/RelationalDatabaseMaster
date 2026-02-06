cf17892372b7c4facc0b5d18bf7ad7f6065d647b
chen tong <chen2015tong@gmail.com>
2026-02-06 22:15:19 +0100

    update jdbc connection driver

--- jdbc-connection-driver/src/main/java/connection/JdbcDriverConnection.java
+++ jdbc-connection-driver/src/main/java/connection/JdbcDriverConnection.java
@@ -7,25 +7,26 @@ import java.util.Properties;
 
 public class JdbcDriverConnection {
 
-    // TODO. 直接通过DB Vendor实现的Driver类型来创建数据库连接
-    // - 使用DriverImpl直接连接到指定的url(包含DB name)
-    // - 配置properties参数列表(User, Password)
     public static void main(String[] args) throws Exception {
         String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
         String url = "jdbc:sqlserver://localhost:1433;Database=test_db;";
+
+        // 为DB链接字符串添加特定属性(User, Password)
         Properties properties = new Properties();
         properties.put("user", "test");
         properties.put("password", "TCHong20");
-        // 等效于在Connection String后面添加配置trustServerCertificate=true
         properties.put("trustServerCertificate", "true");
 
+        // TODO. 通过DB Vendor的Driver Impl具体实现连接到特定数据库
         Driver driver = (Driver) Class.forName(driverName).getDeclaredConstructor().newInstance();
         Connection connection = driver.connect(url, properties);
+
+        // TODO. JDBC Connection默认在SQL Statement结束瞬间会立即更新数据库
         connection.setAutoCommit(true);
 
-        // JDBC Connection默认在SQL Statement结束瞬间会立即更新数据库
-        // 默认或者设置成自动提交的情况下，无需再显式地提交
+        // 默认或者设置成true自动提交时不能再显式commit提交
         // connection.commit();
+
         connection.close();
     }
 
@@ -36,6 +37,7 @@ public class JdbcDriverConnection {
             if (connection == null) {
                 throw new RuntimeException("Can not connect to db of url");
             }
+            connection.close();
         } catch (SQLException exception) {
             String driverClassName = driverObject.getClass().getName();
             if (driverClassName.equals("org.h2.driver")) {
