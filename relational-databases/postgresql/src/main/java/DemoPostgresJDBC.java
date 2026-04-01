232c81417b798afc01787bc6c29720617128cbe0
chent5 <chen2015tong@gmail.com>
2026-04-01 17:19:39 +0200

    refresh project

--- relational-databases/postgresql/src/main/java/DemoPostgresJDBC.java
+++ relational-databases/postgresql/src/main/java/DemoPostgresJDBC.java
@@ -4,17 +4,13 @@ public class DemoPostgresJDBC {
 
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:postgresql://localhost:5432/my_database";
-        try (Connection connection = DriverManager.getConnection(url, "postgres", "")) {
-            connection.getSchema(); // Default Schema: public
+        try (Connection connection = DriverManager.getConnection(url, "postgres", "postgres")) {
+            System.out.println(connection.getSchema());  // Default Schema: public
             System.out.println(connection.getAutoCommit());
 
             Statement statement = connection.createStatement();
             statement.executeUpdate("INSERT INTO t_comment(id, review) values (1002, 'test')");
-
-            Thread.sleep(10000);
             System.out.println("Connection Done");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
     }
 }
