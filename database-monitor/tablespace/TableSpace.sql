2a481cad5aac04066feb9735fa8e35bc493c7990
chent5 <chen2015tong@gmail.com>
2026-06-23 17:22:42 +0200

    refresh tablespace

--- database-monitor/tablespace/TableSpace.sql
+++ database-monitor/tablespace/TableSpace.sql
@@ -1,9 +1,6 @@
 -- Oracle数据库表空间大小检查
-SELECT tablespace_name, file_name, bytes/1024/1024 AS MB
-FROM dba_data_files;
+SELECT tablespace_name, file_name, bytes/1024/1024 AS MB FROM dba_data_files;
 
--- 分区Segment使用的表空间在数据被DELETE后是可被重用
--- 执行TRUNCATE表之后才会释放分区Segment占用的表空间
-SELECT tablespace_name, SUM(bytes)/1024/1024 AS used_mb
-FROM dba_segments
-GROUP BY tablespace_name;
+-- 分区Segment表空间在数据被DELETE后可被重用
+-- 分区Segment表空间在执行TRUNCATE表后自动释放
+SELECT tablespace_name, SUM(bytes)/1024/1024 AS used_mb FROM dba_segments GROUP BY tablespace_name;
